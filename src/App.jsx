f8517ebb2c2f73b48757cf4151a006550bb0072d
Julio Anccasi <julio.anccasi.contractor@bbva.com>
2026-07-28 23:52:52 -0500

    feat: update assets and styles for GitHub User Search App
    - Added new SVG icons for company, location, moon, search, sun, Twitter, and website.
    - Added favicon and octocat image.
    - Removed unused hero image and react SVG.
    - Updated index.css to use Tailwind CSS instead of custom styles.
    - Modified main.jsx to render GitHubUserSearchApp instead of App.
    - Updated Vite configuration to include Tailwind CSS plugin.

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import companyIcon from './assets/icon-company.svg';
+import locationIcon from './assets/icon-location.svg';
+import octocat from './assets/octocat.png';
+import searchIcon from './assets/icon-search.svg';
+import twitterIcon from './assets/icon-twitter.svg';
+import websiteIcon from './assets/icon-website.svg';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const GitHubUserSearchApp = () => {
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <main className="flex flex-col gap-8">
+      <header className="flex justify-between">
+        <h1>devfinder</h1>
+
+        <button className="uppercase text-[#4B6A9B]">Dark</button>
+      </header>
+
+      <form className="flex shadow-[0_16px_30px_-10px_rgba(70,96,187,0.1986)] bg-white py-2 px-3 rounded-2xl">
+        <div className="flex items-center gap-2 w-full">
+          <img className="block w-5 h-5" src={searchIcon} alt="Search icon" />
+
+          <input
+            id="searchInput"
+            className="w-full focus:outline-none"
+            type="search"
+            placeholder="Search GitHub username..."
+          ></input>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
+
+        <button className="bg-[#0079FF] py-3 px-5 rounded-2xl text-white">
+          Search
         </button>
+      </form>
+
+      <section className="bg-white shadow-[0_16px_30px_-10px_rgba(70,96,187,0.1986)] rounded-[15px] py-8 px-6 flex flex-col gap-6">
+        <header className="flex gap-5">
+          <img className="rounded-full" src={octocat} alt="Octocat" />
+
+          <div className="flex flex-col gap-1">
+            <div className="flex flex-col gap-0.5">
+              <h2 className="text-[#2B3442]">The Octocat</h2>
+
+              <h3 className="text-[#0079FF]">@octocat</h3>
+            </div>
+
+            <time className="text-[#4B6A9B]">Joined 25 Jan 2011</time>
+          </div>
+        </header>
+
+        <section className="flex flex-col gap-6">
+          <p className="text-[#4B6A9B]">This profile has no bio</p>
+
+          <section className="bg-[#F6F8FF] py-4 px-5"></section>
+
+          <section className="grid gap-4">
+            <div className="flex gap-4">
+              <img src={locationIcon} alt="Location icon" />
+
+              <p className="text-[#4B6A9B]">San Francisco</p>
+            </div>
+
+            <div className="flex gap-4">
+              <img src={twitterIcon} alt="Twitter icon" />
+
+              <p className="text-[#4B6A9B]">Not Available</p>
+            </div>
+
+            <div className="flex gap-4">
+              <img src={websiteIcon} alt="Website icon" />
+
+              <p className="text-[#4B6A9B]">https://github.blog</p>
+            </div>
+
+            <div className="flex gap-4">
+              <img src={companyIcon} alt="Company icon" />
+
+              <p className="text-[#4B6A9B]">@github</p>
+            </div>
+          </section>
+        </section>
       </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
-
-export default App
+    </main>
+  );
+};
